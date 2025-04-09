e9341e89b92315b54dfb236a3cd57fbbd893c220
ProfessorFranciscoBiudes <127062185+ProfessorFranciscoBiudes@users.noreply.github.com>
2025-04-09 19:13:06 -0300

    OK

--- script.js
+++ script.js
@@ -22,3 +22,10 @@ document.addEventListener('DOMContentLoaded', function () {
     })    
 
 })
+const alternaContraste = document.getElementById('alterna-contraste')
+
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
+
+
